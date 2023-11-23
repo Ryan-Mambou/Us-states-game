72e148aedbc7af25c85fc731277942a60d2149fb
Ryan Djemtchemeu <ryanmambou@gmail.com>
2023-11-23 08:00:47 +0100

    delete uneccessary print and refactoring into list comprehension

--- main.py
+++ main.py
@@ -13,18 +13,16 @@ turtle.shape(image)
 data = pd.read_csv("50_states.csv")
 states = data.state.to_list()
 guessed_states = []
-missing_states = []
+
 
 while len(guessed_states) < 50:
     answer = screen.textinput(title=f"{score_board.score}/50 States correct",
                               prompt="What's another state name ?").title()
 
     if answer == "Exit":
-        for state in states:
-            if state not in guessed_states:
-                missing_states.append(state)
-            new_data_frame = pd.DataFrame(missing_states, columns=["missing states"])
-            new_data_frame.to_csv("missing_states.csv")
+        missing_states = [state for state in states if state not in guessed_states]
+        new_data_frame = pd.DataFrame(missing_states, columns=["missing states"])
+        new_data_frame.to_csv("missing_states.csv")
         break
     if answer in states:
         guessed_states.append(answer)
@@ -35,7 +33,6 @@ while len(guessed_states) < 50:
         t.hideturtle()
         x_coord = data[data["state"] == answer].x
         y_coord = data[data["state"] == answer].y
-        print(x_coord, y_coord)
         t.goto(int(x_coord), int(y_coord))
         t.write(answer)
 
